3493de87fffa9bf6d6bc259a134dd6f2a9997cfd
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2022-01-08 16:11:42 +0100

    [add] mass record save

--- models/record.js
+++ models/record.js
@@ -7,12 +7,53 @@
 const path = require('path')
     , fs = require('fs')
     , yml = require('js-yaml')
-    , moment = require('moment');
+    , moment = require('moment')
+    , slugify = require('slugify');
 
 const Config = require('./config')
     , lang = require('./lang');
 
 module.exports = class Record {
+
+    /**
+     * Save several records from a JSON object
+     * @param {object} data - 
+     * @return {mixed} - Invalid items key or true
+     * @example
+     * Record.massSave({
+     * })
+     */
+
+    static massSave (data) {
+        let i = 1;
+
+        let report = [];
+
+        for (const item of data) {
+            const record = new Record(
+                item.title,
+                item.type,
+                item.tags,
+                item.content,
+                item.filename,
+                Record.generateOutDalyId() + i
+            );
+
+            const result = record.save(true);
+
+            if (result === false) {
+                report.push(i);
+                continue;
+            }
+
+            i++;
+        }
+
+        if (report.length !== 0) {
+            return report }
+
+        return true;
+    }
 
     /**
      * Get a number (14 caracters) from the time stats :
@@ -24,6 +65,11 @@ module.exports = class Record {
         return Number(moment().format('YYYYMMDDHHmmss'));
     }
 
+    static generateOutDalyId () {
+        const maxHour = 24, maxMinute = 60, maxSecond = 60
+        return Number(moment().format('YYYYMMDD') + maxHour + maxMinute + maxSecond);
+    }
+
     /**
      * Generate a record.
      * @param {string} title - Title of the record.
@@ -31,10 +77,11 @@ module.exports = class Record {
      * @param {string} tags - List of tags of the record.
      * @param {string} content - Text content if the record.
      * @param {string} fileName - Text content (Markdown) if the record.
-     * @example new Record('My record', 'concept', 'tag 1,tag 2', 'Lorem *ipsum*', 'my-record');
+     * @param {string} id - Unique identifier of the record.
+     * @example new Record('My record', 'concept', 'tag 1,tag 2', 'Lorem *ipsum*', 'my-record', 20210704100343);
      */
 
-    constructor (title, type = 'undefined', tags = '', content = '', fileName = title) {
+    constructor (title, type = 'undefined', tags = '', content = '', fileName = slugify(title, { remove: /[*+~.()'"!:@]/g }), id = Record.generateId()) {
         /**
          * @type string
          */
@@ -43,7 +90,7 @@ module.exports = class Record {
          * @type number
          * @example 20210704100343
          */
-        this.id = Record.generateId();
+        this.id = id;
         /**
          * @type string
          * @default 'undefined'
@@ -101,7 +148,7 @@ module.exports = class Record {
     save (force = false) {
         try {
             if (this.isValid() === false) {
-                return;
+                return false;
             }
 
             if (this.willOverwrite() === true && force === false) {
@@ -112,6 +159,7 @@ module.exports = class Record {
 
             return true;
         } catch (error) {
+            console.log(error);
             return false;
         }
     }
@@ -123,6 +171,9 @@ module.exports = class Record {
     verif () {
         if (this.title === '') {
             this.report.push('title'); }
+
+        if (new RegExp(/^[0-9]{14}$/gs).test(this.id) === false) {
+            this.report.push('id'); }
 
         if (this.config.record_types[this.type] === undefined) {
             this.report.push('type'); }
