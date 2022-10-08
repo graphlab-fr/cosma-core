7f36ada3fdf84e4807999949de6298a13ccafd24
Myllaume <guillaume.brioudes@myllaume.fr>
2022-10-08 17:33:09 +0200

    edit core tree and scripts

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const fs = require('fs')
-    , path = require('path');
+// const fs = require('fs')
+//     , path = require('path');
 
-const { cosmocope, opensphere } = require('./utils/generate');
-const performance = require('./utils/performance');
+const { cosmocope, opensphere, tempDirPath } = require('./utils/generate');
+// const performance = require('./utils/performance');
 
-const tempDirPath = path.join(__dirname, 'temp');
+// cosmocope(tempDirPath)
+//     .then(({ nbRecords }) => console.log('\x1b[34m', 'Cosmoscope generated', '\x1b[0m', `(${nbRecords} records)`))
+//     .catch(err => console.error('\x1b[31m', 'Err.', '\x1b[0m', err));
 
-cosmocope(tempDirPath)
-    .then(({ nbRecords }) => console.log('\x1b[34m', 'Cosmoscope generated', '\x1b[0m', `(${nbRecords} records)`))
-    .catch(err => console.error('\x1b[31m', 'Err.', '\x1b[0m', err));
+// opensphere(tempDirPath)
+//     .then(({ nbRecords }) => console.log('\x1b[34m', 'Opensphere generated', '\x1b[0m', `(${nbRecords} records)`))
+//     .catch(err => console.error('\x1b[31m', 'Err.', '\x1b[0m', err));
 
-opensphere(tempDirPath)
-    .then(({ nbRecords }) => console.log('\x1b[34m', 'Opensphere generated', '\x1b[0m', `(${nbRecords} records)`))
-    .catch(err => console.error('\x1b[31m', 'Err.', '\x1b[0m', err));
+// performance();
 
-performance();
+const { execute: webpackExecute } = require('./utils/webpack');
+
+webpackExecute('development')
+    .then(() => {
+        cosmocope(tempDirPath)
+            .then(({ nbRecords }) => console.log('\x1b[34m', 'Cosmoscope generated', '\x1b[0m', `(${nbRecords} records)`))
+            .catch(err => console.error('\x1b[31m', 'Err.', '\x1b[0m', err));
+    })
+    .catch((err) => { console.log(err); })
