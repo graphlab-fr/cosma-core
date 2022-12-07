639acc29849ca9c1923f5b3090f30eb5cb5d3717
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2022-12-07 21:29:11 +0100

    edit tags state and sorting

--- frontend/index.js
+++ frontend/index.js
@@ -5,19 +5,4 @@ import './zoom';
 import './bibliography';
 import './timeline';
 import './filter';
-import './keywords';
-
-// const number = [1, 2, 3, 4];
-// console.log([...number]);
-
-// const toto = [
-//     { name: 'Guillaume', age: 24 },
-//     { name: 'Guillaume', age: 24 },
-//     { name: 'Guillaume', age: 24 },
-//     { name: 'Guillaume', age: 24 },
-//     { name: 'Guillaume', age: 24 }
-// ]
-
-// const tata = toto.map(({ name, ...rest }) => {
-//     console.log(rest);
-// })
+import './tags';
