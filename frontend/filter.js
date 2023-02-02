1e61c34bdc35183cfc016edada2c4c749580381e
Myllaume <guillaume.brioudes@myllaume.fr>
2023-02-02 20:49:18 +0100

    edit filter event

--- frontend/filter.js
+++ frontend/filter.js
@@ -4,73 +4,47 @@
  * @copyright GNU GPL 3.0 ANR HyperOtlet
  */
 
-/**
- * @typedef Filter
- * @type {object}
- * @property {boolean} active
- * @property {string[]} nodes
- */
-
-import { displayNodes, hideNodes } from './graph';
-import { setCounters } from './counter';
+import { setNodesDisplaying } from './graph';
 import filterPriority from './filterPriority';
 
-/** @type {string[]} */
-const filtersName = Object.keys(filterList);
-
-/**
- * Toggle a filter from his checkbox
- */
-
-function toggleFilter(filterName) {
-  if (filtersName.includes(filterName) === false) {
-    throw new Error('This filter does not exist.');
-  }
-
-  /** @type {HTMLInputElement} */
-  const input = document.getElementById(`filter-${filterName}`);
-  /** @type {Filter} */
-  const { active, nodes } = filterList[filterName];
-  if (active) {
-    hideNodes(nodes, filterPriority.filteredByType);
-
-    filterList[filterName].active = false;
-    input.checked = false;
-  } else {
-    displayNodes(nodes, filterPriority.filteredByType);
-
-    filterList[filterName].active = true;
-    input.checked = true;
-  }
-
-  setCounters();
-}
-
 window.addEventListener('DOMContentLoaded', () => {
+  /** @type {HTMLFormElement} */
+  const form = document.getElementById('types-form');
   /** @type {HTMLInputElement[]} */
-  const filtersInput = [];
+  const inputs = form.querySelectorAll('input');
 
-  for (const filterName of filtersName) {
-    /** @type {Filter} */
-    const { active } = filterList[filterName];
-    /** @type {HTMLInputElement} */
-    const input = document.getElementById(`filter-${filterName}`);
-    input.checked = active;
+  for (const [name, { active }] of Object.entries(typeList)) {
+    form.querySelector(`[name="${name}"]`).checked = active;
+  }
+  changeTypesState();
 
-    filtersInput.push(input);
+  form.addEventListener('change', changeTypesState);
+
+  function changeTypesState() {
+    let formState = new FormData(form);
+    formState = Object.fromEntries(formState);
+
+    const nodeIdsToDisplay = new Set();
+
+    formState = Object.entries(typeList)
+      .filter(([name]) => !!formState[name])
+      .forEach(([, { nodes }]) => {
+        nodes.forEach((id) => nodeIdsToDisplay.add(id));
+      });
+
+    setNodesDisplaying(Array.from(nodeIdsToDisplay), filterPriority.filteredByType);
   }
 
   let filterNameAltMode;
-  for (const input of filtersInput) {
+  for (const input of inputs) {
     const { name: filterName, checked: active } = input;
 
     input.parentElement.addEventListener('click', (e) => {
-      e.stopPropagation();
-      e.preventDefault();
-
       const altMode = e.altKey;
-
       if (altMode) {
+        e.stopPropagation();
+        e.preventDefault();
+
         if (filterNameAltMode === filterName) {
           displayHidden();
           filterNameAltMode = undefined;
@@ -78,83 +52,24 @@ window.addEventListener('DOMContentLoaded', () => {
           hideAllButOne(filterName);
           filterNameAltMode = filterName;
         }
-      } else {
-        toggleFilter(filterName);
-        filterNameAltMode = undefined;
       }
     });
   }
 
-  /**
-   * Display one filter and hide others
-   * @param {string} oneFilterName
-   */
-
-  function hideAllButOne(oneFilterName) {
-    /** @type {HTMLInputElement|undefined} */
-    let oneFilterInput;
-
-    /**
-     * Hide all
-     */
-    const nodesToHide = [];
-    for (const input of filtersInput) {
-      const { name: filterName, checked: active } = input;
-      if (filterName === oneFilterName) {
-        oneFilterInput = input;
-        continue;
-      }
-      if (active === false) {
-        continue;
-      }
-
-      /** @type {Filter} */
-      const { nodes } = filterList[filterName];
-      nodesToHide.push(...nodes);
-
-      filterList[filterName].active = false;
-      input.checked = false;
-    }
-
-    hideNodes(nodesToHide, filterPriority.filteredByType);
-
-    /**
-     * Display one
-     */
-    if (filterList[oneFilterName].active === false) {
-      /** @type {Filter} */
-      const { nodes } = filterList[oneFilterName];
-      displayNodes(nodes, filterPriority.filteredByType);
-
-      filterList[oneFilterName].active = true;
-      oneFilterInput.checked = true;
-    }
-
-    setCounters();
-  }
-
-  /**
-   * Display nodes from deactivated filters
-   */
+  hotkeys('alt+r', (e) => {
+    e.preventDefault();
+    displayHidden();
+  });
 
   function displayHidden() {
-    const nodesToDisplay = [];
-    for (const input of filtersInput) {
-      const { name: filterName, checked: active } = input;
-      if (active) {
-        continue;
-      }
+    form
+      .querySelectorAll(`input:not(:checked)`)
+      .forEach((checkedInput) => (checkedInput.checked = true));
+    form.dispatchEvent(new Event('change'));
+  }
 
-      /** @type {Filter} */
-      const { nodes } = filterList[filterName];
-      nodesToDisplay.push(...nodes);
-
-      filterList[filterName].active = true;
-      input.checked = true;
-    }
-
-    displayNodes(nodesToDisplay, filterPriority.filteredByType);
-
-    setCounters();
+  function hideAllButOne(filterName) {
+    inputs.forEach((input) => (input.checked = filterName === input.name));
+    form.dispatchEvent(new Event('change'));
   }
 });
