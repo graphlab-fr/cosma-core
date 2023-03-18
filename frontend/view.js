bc7a95431be955764269af0119e53d3f0699c71b
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2023-03-18 15:28:13 +0100

    add save view in url

--- frontend/view.js
+++ frontend/view.js
@@ -8,15 +8,30 @@ export default class View {
   static openedRecordId = undefined;
 
   static position = { x: 0, y: 0, zoom: 1 };
-
-  static reset() {
-    console.log('reset view');
-    // if (View.openedRecordId) { closeRecord(); }
-    // zoomReset();
-    // activeAllFilters();
-    // if (focus.isActive) { focus.disable(); }
-    // unactiveAllTags();
-    // displayAllFromIndex();
-    // unactiveLastView();
-  }
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('view-save').addEventListener('click', () => {
+    const url = new URL(window.location);
+
+    const activeFilters = Array.from(document.querySelectorAll('#types-form input:checked')).map(
+      ({ name }) => name
+    );
+    const activeTags = Array.from(document.querySelectorAll('#tags-form input:checked')).map(
+      ({ name }) => name
+    );
+    const focusLevel = document.getElementById('focus-input').value;
+
+    if (activeFilters.length > 0) {
+      url.searchParams.set('filters', activeFilters.join('-'));
+    }
+    if (activeTags.length > 0) {
+      url.searchParams.set('tags', activeTags.join('-'));
+    }
+    if (document.querySelector('#focus-check:checked')) {
+      url.searchParams.set('focus', focusLevel);
+    }
+
+    window.location.replace(url);
+  });
+});
